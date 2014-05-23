76ffab69514d1d7a49bac78541d99dde870345a7
Githubshanna <shannahutch@gmail.com>
2014-05-23 14:02:22 -0700

    needs work commit

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,3 +1,37 @@
 class PinsController < ApplicationController
-  resources :pins
+  # resources :pins
+
+  def new
+   
+    @pin = Pin.new
+  end
+
+  def index 
+    @pin = Pin.all
+    resopnd_to do |f|
+      f.html {render :index}
+      j.json { render :json => @pins, only: [:id, :latitude, :longitude, :http_response]}
+    end
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    respond_to do |format|
+      if @pin.save
+        format.json { render json: @pin, status: :created}
+      else
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+
+  private
+  def pin_params
+    params.require(:pin).permit(:latitude, :longitude)
+  end
+
+
+
 end
